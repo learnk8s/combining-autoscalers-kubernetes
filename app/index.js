b0faf295bcd5e7f4c6f6ff9c408b2e0af15ad267
Daniele Polencic <daniele.polencic@gmail.com>
2022-08-26 09:41:18 +0800

    fixed rate limiting, added signals

--- app/index.js
+++ app/index.js
@@ -39,15 +39,31 @@ console.log(`Starting server http://localhost:${port} with ${rps} rps`);
 
 setInterval(() => {
   requestsForCurrentWindow = 0;
-}, 1000 / rps);
+}, 1000);
 
 function reply(res, retryAttempt = 0) {
   if (requestsForCurrentWindow > rps) {
-    setTimeout(
-      () => reply(res, retryAttempt + 1),
-      Math.pow(2, retryAttempt) * 1000
-    );
+    const ms = Math.pow(2, retryAttempt) * 1000;
+    console.log(`RATE LIMITED, waiting ${ms / 1000} seconds`);
+    setTimeout(() => reply(res, retryAttempt + 1), ms);
   } else {
     res.end("OK");
   }
 }
+
+process.on("SIGINT", async () => {
+  console.log("[SIGINT]");
+  process.exit(0);
+});
+process.on("SIGTERM", async () => {
+  console.log("[SIGTERM]");
+  process.exit(0);
+});
+process.on("SIGHUP", async () => {
+  console.log("[SIGHUP]");
+  process.exit(0);
+});
+process.on("uncaughtException", async (error) => {
+  console.log("[uncaughtException] ", error);
+  process.exit(1);
+});
